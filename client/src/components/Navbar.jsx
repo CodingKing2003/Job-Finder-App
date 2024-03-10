dc22e8fc10094f0538ea6b9e41e36145780c81f9
Rajan <rajanj448@student.sfit.ac.in>
2024-03-10 22:01:55 +0530

    React Router Dom

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { Fragment, useState } from "react";
+import { Menu, Transition } from "@headlessui/react";
+import { BiChevronDown } from "react-icons/bi";
+import { CgProfile } from "react-icons/cg";
+import { HiMenuAlt3 } from "react-icons/hi";
+import { AiOutlineClose, AiOutlineLogout } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
